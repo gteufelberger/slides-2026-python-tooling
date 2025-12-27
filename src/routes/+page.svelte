e6ebdcc2f9be80147b691c339ad964da2d00054c
Georg Teufelberger <georg.teufelberger@esa.int>
2025-12-27 01:02:24 +0100

    feat: Add initial slides skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import Reveal from "reveal.js";
+  import "reveal.js/dist/reveal.css";
+  import "reveal.js/dist/theme/black.css"; // Choose your theme
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    Reveal.initialize({});
+  });
+</script>
+
+<div class="reveal">
+  <div class="slides">
+    <section>
+      <h1>Hello, World!</h1>
+      <p>First slide</p>
+    </section>
+
+    <section>
+      <h2>Second Slide</h2>
+      <ul>
+        <li>Point 1</li>
+        <li>Point 2</li>
+      </ul>
+    </section>
+  </div>
+</div>
+
+<style>
+</style>
