458ff377d81b388624213205f2a3b5322ac0391e
Georg Teufelberger <georg.teufelberger@esa.int>
2025-12-29 12:21:29 +0100

    feat: Add feedback section content

--- src/lib/sections/FeedbackSection.svelte
+++ src/lib/sections/FeedbackSection.svelte
@@ -1,3 +1,22 @@
 <section>
   <h2>Feedback?</h2>
+  <section>
+    <ul>
+      <li>What was new?</li>
+      <li>What did you already know?</li>
+    </ul>
+  </section>
+  <section>
+    Requests on future topics?
+    <br />
+    E.g.
+    <ul>
+      <li>Testing with pytest</li>
+      <li>Introduction to Rust</li>
+      <li>CI/CD pipelines crash course</li>
+      <li>Renting and deploying servers on ESA infrastructure</li>
+      <li>git deep dive</li>
+      <li>...</li>
+    </ul>
+  </section>
 </section>
