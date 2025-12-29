bdc35df3d2d87d3ff874d4109623b2db88e3d898
Georg Teufelberger <georg.teufelberger@esa.int>
2025-12-29 12:11:35 +0100

    feat: Add note about requirements.txt and pyproject.toml

--- src/lib/sections/DependencyManagementSection.svelte
+++ src/lib/sections/DependencyManagementSection.svelte
@@ -4,5 +4,19 @@
   <section>pip</section>
   <section>conda</section>
   <section>poetry</section>
+  <section>
+    A note on requirements.txt
+    <ul>
+      <li>requirements.txt is NOT a standard!</li>
+      <li>It's merely a convention</li>
+    </ul>
+  </section>
+  <section>
+    <h3>PEP621</h3>
+    <ul>
+      <li>Python standard released in 2020</li>
+      <li>Goodbye `pip install -r requirements.txt`, hello `pip install .`</li>
+    </ul>
+  </section>
   <section>uv</section>
 </section>
