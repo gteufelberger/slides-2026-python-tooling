e39b977a61ed63c9e3c93d1ae7adc7309f2ac22e
Georg Teufelberger <georg.teufelberger@esa.int>
2025-12-29 11:16:03 +0100

    feat: Add QR code to link to slides

--- src/lib/TitleSlideContent.svelte
+++ src/lib/TitleSlideContent.svelte
@@ -1,6 +1,15 @@
+<script>
+  import qrCode from "$lib/assets/qr-code-link.png";
+</script>
+
 <h1>Python Tooling in 2026</h1>
 <p>
   What's new? What's good?
   <br />
   Tips and tricks
 </p>
+
+<div>
+  <span>Link to slides:</span>
+  <img src={qrCode} alt="QR code link to slides" class="qr-code" />
+</div>
